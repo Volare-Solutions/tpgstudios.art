0a67e2c010048e7eb12d6772d931e3764b608de8
grayson40 <graysoncrozier40@gmail.com>
2024-03-15 14:30:08 -0400

    Revert "Profile page changes"
    This reverts commit e953e671f0123c6eb57c8915e1a096d604bce236.

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,15 +1,7 @@
-<script lang="ts">
-	import type { PageData } from "./$types.js";
-	import ProfileForm from "./profile-form.svelte";
-	import { Separator } from "$lib/components/ui/separator/index.js";
-	export let data: PageData;
+<script>
+	import { Button } from '$lib/components/ui/button';
 </script>
 
-<div class="space-y-6">
-	<div>
-		<h3 class="text-lg font-medium">Account</h3>
-		<p class="text-sm text-muted-foreground">This is how others will see you on the site.</p>
-	</div>
-	<Separator />
-	<ProfileForm data={data.form} />
-</div>
+<div>profile page coming soon!</div>
+
+<Button href="/auth/logout">Logout</Button>
