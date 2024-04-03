dc29288d8826c4da6268af70e4cbe582913b2513
grayson40 <graysoncrozier40@gmail.com>
2024-04-02 21:21:10 -0400

    Update email page styling

--- src/routes/auth/list/+page.svelte
+++ src/routes/auth/list/+page.svelte
@@ -5,22 +5,24 @@
 	import { Label } from '$lib/components/ui/label';
 </script>
 
-<Card.Root class="w-[400px] mb-12">
-	<Card.Header>
-		<Card.Title>Email List</Card.Title>
-		<Card.Description>Get updates and deals from the TPG team</Card.Description>
-	</Card.Header>
-	<Card.Content>
-		<form method="POST">
-			<div class="grid w-full items-center gap-4">
-				<div class="flex flex-col space-y-1.5">
-					<Label for="email">Email</Label>
-					<Input id="email" placeholder="you@mail.com" name="email" />
+<div class="flex justify-center mt-32">
+	<Card.Root class="w-[400px] mb-12">
+		<Card.Header>
+			<Card.Title>Email List</Card.Title>
+			<Card.Description>Get updates and deals from the TPG team</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<form method="POST">
+				<div class="grid w-full items-center gap-4">
+					<div class="flex flex-col space-y-1.5">
+						<Label for="email">Email</Label>
+						<Input id="email" placeholder="you@mail.com" name="email" />
+					</div>
 				</div>
-			</div>
-			<div class="flex justify-end pt-4">
-				<Button type="submit">Join</Button>
-			</div>
-		</form>
-	</Card.Content>
-</Card.Root>
+				<div class="flex justify-end pt-4">
+					<Button type="submit">Join</Button>
+				</div>
+			</form>
+		</Card.Content>
+	</Card.Root>
+</div>
