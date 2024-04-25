ff776ecd1980516dceb52f6166a409b72dca8b26
grayson40 <graysoncrozier40@gmail.com>
2024-04-25 19:53:38 -0400

    Update auth email list

--- src/routes/auth/list/+page.server.ts
+++ src/routes/auth/list/+page.server.ts
@@ -1,34 +1,54 @@
 import { db } from '$lib/server/db/index.js';
 import { emailList } from '$lib/server/db/schema.js';
-import { error, redirect } from '@sveltejs/kit';
+import { error, redirect, fail } from '@sveltejs/kit';
 import { generateId } from 'lucia';
 import { zfd } from 'zod-form-data';
 import { sendThankYouListEmail } from '$lib/server/resend';
+import { eq } from 'drizzle-orm';
 
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
 
 		const schema = zfd.formData({
-			email: zfd.text()
+			email: zfd.text(),
+			phone: zfd.text(),
+			size: zfd.text()
 		});
 
 		const res = schema.safeParse(data);
 
 		if (!res.success) {
-			error(400, res.error.name);
+			return fail(400, {
+				error: 'Invalid form submission',
+			});
+		}
+
+		// Check if email already exists
+		const existingEmail = await db.select().from(emailList).where(eq(emailList.email, res.data.email));
+		if (existingEmail.length > 0) {
+			return fail(400, {
+				error: 'Email already exists',
+			});
 		}
 
 		const key = generateId(20);
 
 		await db.insert(emailList).values({
 			email: res.data.email,
+			phoneNumber: res.data.phone,
+			shirtSize: res.data.size,
 			key,
 			subscribedAt: new Date()
 		});
 
 		await sendThankYouListEmail(res.data.email, key);
 
-		redirect(303, '/');
+		return {
+			status: 200,
+			body: {
+				message: 'Success'
+			}
+		}
 	}
 };
--- src/routes/auth/list/+page.svelte
+++ src/routes/auth/list/+page.svelte
@@ -3,20 +3,60 @@
 	import * as Card from '$lib/components/ui/card';
 	import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
+	import { enhance } from '$app/forms';
+	import { Check, AlertTriangle } from 'lucide-svelte';
+	import * as Alert from '$lib/components/ui/alert';
+	import { fade } from 'svelte/transition';
+
+	let showSuccess = false;
+	let showError = false;
+	let errorMessage = '';
+	let formElement: HTMLFormElement;
 </script>
 
-<div class="flex justify-center mt-32">
+<div class="flex justify-center mt-24 md:mt-32">
 	<Card.Root class="w-[400px] mb-12">
 		<Card.Header>
 			<Card.Title>Email List</Card.Title>
 			<Card.Description>Get updates and deals from the TPG team</Card.Description>
 		</Card.Header>
 		<Card.Content>
-			<form method="POST">
+			<form method="POST" bind:this={formElement} use:enhance={() => {
+				return ({ result }) => {
+					if (result.status === 200) {
+						showSuccess = true;
+						formElement.reset();
+						setTimeout(() => {
+							showSuccess = false;
+						}, 3000);
+					}
+					if (result.status === 400) {
+						errorMessage = String(result.data?.error);
+						showError = true;
+						setTimeout(() => {
+							showError = false;
+						}, 3000);
+					}
+				};
+			}}>
 				<div class="grid w-full items-center gap-4">
 					<div class="flex flex-col space-y-1.5">
 						<Label for="email">Email</Label>
 						<Input id="email" placeholder="you@mail.com" name="email" />
+					</div>
+					<div class="flex flex-col space-y-1.5">
+						<Label for="phone">Phone Number</Label>
+						<Input id="phone" placeholder="999-999-9999" name="phone" />
+					</div>
+					<div class="flex flex-col space-y-1.5">
+						<Label for="size">Shirt Size</Label>
+						<select id="size" name="size">
+							<option value="">Select size</option>
+							<option value="S">Small</option>
+							<option value="M">Medium</option>
+							<option value="L">Large</option>
+							<option value="XL">Extra Large</option>
+						</select>
 					</div>
 				</div>
 				<div class="flex justify-end pt-4">
@@ -26,3 +66,27 @@
 		</Card.Content>
 	</Card.Root>
 </div>
+
+{#if showSuccess}
+	<div class="absolute md:bottom-8 md:right-8 w-full md:w-[500px] z-50 flex justify-center md:block" transition:fade>
+		<div class="w-[90%] md:w-full">
+			<Alert.Root>
+				<Check class="h-4 w-4" />
+				<Alert.Title>Success!</Alert.Title>
+				<Alert.Description>Thank you for joining our mailing list.</Alert.Description>
+			</Alert.Root>
+		</div>
+	</div>
+{/if}
+
+{#if showError}
+	<div class="absolute md:bottom-8 md:right-8 w-full md:w-[500px] z-50 flex justify-center md:block" transition:fade>
+		<div class="w-[90%] md:w-full">
+			<Alert.Root>
+				<AlertTriangle class="h-4 w-4" />
+				<Alert.Title>Error!</Alert.Title>
+				<Alert.Description>{errorMessage}</Alert.Description>
+			</Alert.Root>
+		</div>
+	</div>
+{/if}
