b9c4076c112871f1905d3bb32b2fa7886b6829ca
grayson40 <graysoncrozier40@gmail.com>
2024-11-11 11:29:48 -0500

    Update order success msg

--- src/routes/status/checkout/success/+page.svelte
+++ src/routes/status/checkout/success/+page.svelte
@@ -1,18 +1,18 @@
 <div class="h-[80vh] flex items-center justify-center flex-col md:w-3/5 m-auto gap-4 px-4">
-	<h2 class="font-jura sm:text-4xl text-3xl text-center py-3">Thank You for Ordering!</h2>
-	<p class="font-jura">
-		You are one of the very first (among the first 10) to order from us, and we could not be more
-		grateful. We have included for free an exclusive piece from a currently unreleased collection
-		that will be sent with your order.
-	</p>
-	<p class="font-jura">
-		We're thrilled to be getting one of our pieces in your hands. We'll keep you updated as we get
-		your order fulfilled.
-	</p>
+    <h2 class="font-jura sm:text-4xl text-3xl text-center py-3">Thank You for Ordering!</h2>
+    <p class="font-jura">
+        Thank you for supporting our independent brand. As a token of our appreciation, we've included a 
+        special surprise with your order - an exclusive piece from our upcoming collection.
+    </p>
+    <p class="font-jura">
+        We're thrilled to be getting one of our pieces in your hands. We'll keep you updated throughout
+        the fulfillment process.
+    </p>
 
-	<p class="font-jura">
-		Remember that our pieces are made to order, so expect at least a week before your's arrives.
-	</p>
+    <p class="font-jura">
+        Since our pieces are made to order with care and attention to detail, please allow at least a week
+        for your order to arrive.
+    </p>
 
-	<p class="font-jura">Keep an eye on your email, we'll send you updates!</p>
+    <p class="font-jura">Keep an eye on your email for updates on your order's journey!</p>
 </div>
